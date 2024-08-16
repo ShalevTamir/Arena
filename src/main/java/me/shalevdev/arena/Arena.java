1614e5061f7d06d2bec210533194b89e3d8f98d6
shalevdev <shalevVarietyDeveloper@gmail.com>
2024-08-16 16:06:21 +0300

    Initial commit

---
+++ src/main/java/me/shalevdev/arena/Arena.java
@@ -0,0 +1,14 @@
+package me.shalevdev.arena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Arena extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
